0eb6e5ee515808e72c418c82feeef7fe8a59bf30
Felipe Homrich Melchior <fehmel@gmail.com>
2019-10-23 12:17:21 -0300

    feat: add global styles

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import './config/ReactotronConfig';
 import history from './services/history';
 import Routes from './routes';
 
+import GlobalStyles from './styles/global';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyles />
     </Router>
   );
 }
