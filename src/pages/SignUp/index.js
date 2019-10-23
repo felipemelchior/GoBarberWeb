ec5f7a43d22f9c957ceaf49e82b97a211cf220c7
Felipe Homrich Melchior <fehmel@gmail.com>
2019-10-23 12:50:26 -0300

    feat: add signin/signup stylization

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import Logo from '~/assets/logo.svg';
 
-export default function SignUp() {
-  return <div />;
+export default function SignIn() {
+  return (
+    <>
+      <img src={Logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Seu nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
