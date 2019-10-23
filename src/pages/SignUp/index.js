f92070a545131580580bfa433ddec1185bbc48ed
Felipe Homrich Melchior <fehmel@gmail.com>
2019-10-23 14:14:33 -0300

    feat: add unform validation

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import Logo from '~/assets/logo.svg';
 
-export default function SignIn() {
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
+export default function SignUp() {
   function handleSubmit(data) {
     console.tron.log(data);
   }
@@ -13,7 +24,7 @@ export default function SignIn() {
     <>
       <img src={Logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Seu nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
