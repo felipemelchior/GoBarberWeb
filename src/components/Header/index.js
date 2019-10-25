0a00385d50ad2cce105c2d4c0534cf38d8e5d485
Felipe Homrich Melchior <fehmel@gmail.com>
2019-10-25 12:17:58 -0300

    feat: add header stylization

---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logoHeader.svg';
+
+import { Container, Content, Profile } from './styles';
+
+import Notifications from '~/components/Notifications';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+
+          <Profile>
+            <div>
+              <strong>Felipe Melchior</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Avatar"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
